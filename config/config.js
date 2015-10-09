d89685bf6594ce518dc0cc554fb42ef4d434f4a0
jurporan <jan.purro@heig-vd.ch>
2015-10-09 15:15:33 +0200

    création squelette application

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'teaching-heigvd-tweb-2015-project'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/teaching-heigvd-tweb-2015-project-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'teaching-heigvd-tweb-2015-project'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/teaching-heigvd-tweb-2015-project-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'teaching-heigvd-tweb-2015-project'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/teaching-heigvd-tweb-2015-project-production'
+  }
+};
+
+module.exports = config[env];
