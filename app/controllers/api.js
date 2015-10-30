f1b79faceac776177fb3f9a4de4db8c42cc3b9ff
Jérôme Moret <jermoret@gmail.com>
2015-10-30 14:40:04 +0100

    Implémentation /api/poll

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,72 +1,89 @@
-var express = require('express');
-router = express.Router();
+var express = require('express'),
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  Poll = mongoose.model('Poll');
 
-module.exports = function (app)
-{
+module.exports = function (app) {
   app.use('/api', router);
 };
 
 // GET
 
-router.get('/poll/:pollid/question/:questionid/results', function (req, res)
-{
+router.get('/poll/:pollid/question/:questionid/results', function (req, res) {
   res.format(
-  {
-    'application/json': function()
     {
-      res.send("Envoyer du json ici!!!");
-    }
-  });
+      'application/json': function () {
+        res.send({});
+      }
+    });
 });
 
-router.get('/poll/:pollid/question/:questionid', function (req, res)
-{
+router.get('/poll/:pollid/question/:questionid', function (req, res) {
   res.format(
-  {
-    'application/json': function()
     {
-      res.send("Envoyer du json ici!!!");
-    }
-  });
+      'application/json': function () {
+        res.send("Envoyer du json ici!!!");
+      }
+    });
 });
 
-router.get('/polls/:type', function (req, res)
-{
+router.get('/polls/:type', function (req, res) {
   res.format(
-  {
-    'application/json': function()
     {
-      res.send("Envoyer du json ici!!!");
-    }
-  });
+      'application/json': function () {
+        res.send("Envoyer du json ici!!!");
+      }
+    });
 });
 
-router.get('/poll/:pollid', function (req, res)
-{
+router.get('/poll/:pollid', function (req, res) {
   res.format(
-  {
-    'application/json': function()
     {
-      res.send("Envoyer du json ici!!!");
-    }
-  });
+      'application/json': function () {
+        res.send("Envoyer du json ici!!!");
+      }
+    });
 });
 
-router.get('/poll', function (req, res)
-{
-  res.format(
-  {
-    'application/json': function()
-    {
-      res.send("Envoyer du json ici!!!");
-    }
-  });
+router.get('/poll', function (req, res) {
+
+  var response = {};
+  Poll.count({
+      $or: [
+        {state: "active"},
+        {state: "drafti"}
+      ]
+    },
+    function (err, nb_open) {
+      if (err) throw next(err);
+
+      Poll.count({state: "closed"},
+        function (err, nb_closed) {
+          if (err) throw err;
+
+          var sinceDate = new Date(req.query.since);
+          Poll.count({creationDate: {$gt: sinceDate}},
+            function (err, nb_recent) {
+              if (err) throw err;
+              response.nb_open = nb_open;
+              response.nb_closed = nb_closed;
+              response.nb_recent = nb_recent;
+
+              res.format(
+                {
+                  'application/json': function () {
+                    res.send(response);
+                  }
+                });
+
+            });
+        });
+    });
 });
 
 // POST
 
-router.post('/poll', function (req, res)
-{
-    var data = req.body;
-    res.send("Envoyer du json ici!!! " + data.name);
+router.post('/poll', function (req, res) {
+  var data = req.body;
+  res.send("Envoyer du json ici!!! " + data.name);
 });
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -11,27 +11,7 @@ module.exports = function (app) {
 };
 
 router.get('/', function (req, res, next) {
-  Poll.count(function(err, totPolls) {
-    if(err) throw next(err);
-
-    var actualDate = new Date(Date.now());
-    Poll.count({ creationDate: {$gt: new Date().setDate(actualDate.getDate() - 7)} },
-    function(err, totWeekPolls) {
-      if(err) throw err;
-
-      Poll.count({ $or: [
-          {state: "active"},
-          {state: "drafti"}
-        ]},
-      function(err, totOpenPolls) {
-          if(err) throw next(err);
-          res.render('index', {
-            title: 'PollY',
-            pollsTot: totPolls,
-            pollsTotWeek: totWeekPolls,
-            pollsTotOpen: totOpenPolls
-          });
-      });
-    });
+  res.render('index', {
+    title: 'Generator-Express MVC'
   });
 });
