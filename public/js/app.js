3923833a6c6fcc5f07ce799fce9b417e5c8f1f25
benoistwolleb <planetben1@gmail.com>
2015-11-06 14:44:49 +0100

    Angularized

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+var qpoll = angular.module('qpoll', []);
+
+qpoll.controller("statController", function($http, $scope)
+{
+    $http.get("/api/poll").then(function(response)
+    {
+        $scope.total = response.data.nb_open + response.data.nb_closed;
+        $scope.recent = response.data.nb_recent;
+        $scope.open = response.data.nb_open;
+    }).then(function(response)
+    {
+        alert("Error! " + response.statusText);
+    });
+});
