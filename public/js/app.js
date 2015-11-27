f56b3b12d81a953d65ff2d6ac8bc077602687c3d
melhk <melanie.huck@alumni.epfl.ch>
2015-11-27 14:26:54 +0100

    liste questionnaires

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,45 @@
 var northPoll = angular.module('northPoll', []);
 
-northPoll.controller("statController", function($http, $scope)
-{
-    $http.get("/api/poll").then(function(response)
-    {
+
+// ajouter un service
+northPoll.factory('pollManager', function($http) {
+    return {
+        getPolls: function() {
+            // retourner un tableau
+            return [
+                {
+                  name : "poll1",
+                  description: "a poll for demo"
+                 },
+                {
+                  name : "poll2",
+                  description: "a poll for demo 2"
+                 },
+                {
+                  name : "poll3",
+                  description: "a poll for demo 3"
+                 },
+                 
+            ];
+        }
+     };
+ });
+
+
+northPoll.controller("statController", function($http, $scope) {
+    $http.get("/api/poll").then(function(response){
         $scope.total = response.data.nb_open + response.data.nb_closed;
         $scope.recent = response.data.nb_recent;
         $scope.open = response.data.nb_open;
-    }).then(function(response)
-    {
+    }).then(function(response){
         alert("Error! " + response.statusText);
     });
-});
+    
+ });
+ 
+ northPoll.controller("pollsController", function(pollManager, $scope) {
+    $scope.polls = pollManager.getPolls();
+
+    
+ });
+
