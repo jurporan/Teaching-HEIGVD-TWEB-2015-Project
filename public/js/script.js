bfe99115032b36a3aa17a25bc2369e72e9df3750
Jérôme Moret <jermoret@gmail.com>
2015-12-04 15:04:06 +0100

    Change structure of the page

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,6 @@
 /**
  * Created by jermoret on 03.12.2015.
  */
-$('.clicker').click(function(){
-  $(this).nextUntil('.clicker').slideToggle('normal');
-});
-
 // Activate Carousel
 $("#myCarousel").carousel({interval: false});
 
